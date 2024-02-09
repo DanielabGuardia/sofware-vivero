47149d7184631e89f85087a8a9e79f93195971af
DanielaGuardia <danielabguardia@gmail.com>
2024-02-08 22:38:10 -0300

    node-modules

---
+++ src/controllers/homeController.js
@@ -0,0 +1,42 @@
+//let db = require('../database/models')
+/*let Products = db.Product
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+let controller = {
+    index:function (req, res) {
+        Products.findAll()
+        .then(products =>{
+            res.render('home',{
+                products,
+                toThousand,
+                session: req.session
+            })
+        })
+        
+        
+    },
+    preguntas_frecuentes : (req,res)=>{
+        res.render('frequent_questions',{
+            session: req.session
+        })
+    },
+    locals : (req,res)=>{
+        res.render('locals',{
+            session: req.session
+        })
+    }
+
+}
+
+module.exports = controller*/
+
+
+const homeController = {
+    home: (req, res) => {
+       res.render('views/home');
+    } ,
+    
+   
+   };
+    
+   module.exports = homeController;  
